f4e5219bb117447a0d18652aade45bceae8d4140
Shashank Singh <146385389+Rajput7272@users.noreply.github.com>
2024-12-05 13:07:26 +0530

    Add files via upload

---
+++ StopWatch App/App.jsx
@@ -0,0 +1,14 @@
+import{Routes,Route} from 'react-router'
+
+const App=()=>{
+    return(
+        <div>
+            <Routes>
+                <Route path="/" element ={<h1>Home Page</h1>}></Route>
+                <Route path="/" element ={<h1>Home Page</h1>}></Route>
+                <Route path="/" element ={<h1>Home Page</h1>}></Route>
+                <Route path="/" element ={<h1>Home Page</h1>}></Route>
+            </Routes>
+            </div>
+    )
+}
