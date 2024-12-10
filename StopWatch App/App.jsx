4f684976a9e150bdeac233dd16a57d46440ed711
Shashank Singh <146385389+Rajput7272@users.noreply.github.com>
2024-12-10 13:08:48 +0530

    Update App.jsx

--- StopWatch App/App.jsx
+++ StopWatch App/App.jsx
@@ -1,14 +1,16 @@
 import{Routes,Route} from 'react-router'
-
+import Refex from'./Component/Refex'
 const App=()=>{
     return(
         <div>
             <Routes>
                 <Route path="/" element ={<h1>Home Page</h1>}></Route>
-                <Route path="/" element ={<h1>Home Page</h1>}></Route>
-                <Route path="/" element ={<h1>Home Page</h1>}></Route>
-                <Route path="/" element ={<h1>Home Page</h1>}></Route>
+                <Route path="/counter" element ={<h1>Counter app</h1>}></Route>
+                <Route path="/refex" element ={<Refex/>}></Route>
+                <Route path="/stopwatch" element ={<h1>Stopwatch App</h1>}></Route>
+                <Route path="*" element ={<h1>No Page Avialable</h1>}></Route>
             </Routes>
             </div>
     )
 }
+export default App;
